6a9c50bc67d67106cbe277da56a8d460c32cb584
Erik Rose <grinch@grinchcentral.com>
2015-08-22 20:55:27 -0400

    Get whitespace-insensitive grammar passing smoketests.
    * Remove the indentation-sensitive stuff.
    * Tolerate whitespace after paths.
    * Disallow quotes in barewords, and move hypen to the front of the char class so we don't include an unintended range.
    * Flop the order of word and text alternatives in item production, not that it really matters since now that have no intersection.
    
    Next: comments.

--- parsing.py
+++ parsing.py
@@ -9,21 +9,20 @@ from parsimonious import Grammar
 grammar = Grammar(r"""
     list = bracketed_list  # / block
     bracketed_list = "[" _ item* "]" _
-    block = ~r"\n" dent
 
     # Parens are just like lists but can be treated differently by a dialect.
-    # For example, the "do" dialect evaluates them immediately rather than
+    # For example, Rebol's "do" dialect evaluates them immediately rather than
     # lazily.
     paren = "(" _ item* ")" _
 
     # An item is an element of a list.
-    item = path / number / word / text / list / paren
+    item = path / number / text / word / list / paren
 
     # Words are barewords, unquoted things, other than literals, that can live
     # in lists. We may renege on some of these chars later, especially ".". We
     # may add Unicode.
     word = spaceless_word _
-    spaceless_word = ~r"[a-z`~!@#$%^&*-_+=|\\:;\"'<>,.?][a-z0-9`~!@#$%^&*-_+=|\\:;\"'<>,.?]*"i
+    spaceless_word = ~r"[-a-z`~!@#$%^&*_+=|\\:;<>,.?][-a-z0-9`~!@#$%^&*_+=|\\:;<>,.?]*"i
 
     # Strings are multiline atm. This may change if we find a reason.
     text = spaceless_text _
@@ -35,7 +34,7 @@ grammar = Grammar(r"""
     spaceless_int = ~r"[0-9]+"
     decimal = ~r"[0-9]*\.?[0-9]+" _
 
-    path = path_item ("/" path_item)+
+    path = path_item ("/" path_item)+ _
     path_item = spaceless_word / spaceless_text / spaceless_int
 
     _ = ~r"[ \t]*"
@@ -45,29 +44,3 @@ grammar = Grammar(r"""
 
 def parse(text):
     return grammar.parse(text)
-
-
-dent_re = re.compile(r'[ \t]*(?=\S)')
-class IndentTracker(object):
-    def __init__(self):
-        self.dents = []  # a stack of ints, each being the number of whitespace chars indenting the line at that indentation level
-        self.pos = 0
-        self.level = 0  # number of whitespace chars the line beginning at ``self.pos`` is indented. This is used to yield matches on successive dedent() calls iff a line dedents more than one notch.
-
-    @expression
-    def dedent(self, text, pos):
-        match = dent_re.match(text, pos)  # Optimization: move this to a Regex expr that's a member of this so it can be cached. Make this a Compound subclass.
-        if match:
-            level = len(match.group(0))
-            
-            if level < self.dents
-# Maybe it'd be simpler and faster just to have a pre-phase that .splitlines() the whole source and stores a map of which lines dent, which way, and how much. Then the custom rule can just do a lookup on that, plus it can be cacheable.
-
-grammar = Grammar(r"""
-    block = indent clause (clause / block)* dedent
-    clause = samedent somestuff
-    """,
-    dedent = 
-    
-
-# If we're going to have stateful custom rules, those are context-sensitive and cannot be cached. Neither can anything that depends on them be cached, so we have to percolate the _is_cacheable = False up during compilation.
--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,16 +1,43 @@
-from nose.tools import eq_
+from nose.tools import eq_, assert_raises
+from parsimonious import ParseError
+from parsimonious.nodes import Node, RegexNode
 
-from turtles.parsing import parse
+from turtles.parsing import parse, grammar
+
+# To turn into tests:
+"""
+'[deferred: thing]
+'[8]  --comment
+"""
+# Bracketed constructs should ultimately lose their brackets:
+# show: "hey"
+# frob: thing with: thong
+# 1 + 2
+
+# Quoted bracketed constructs should get just brackets:
+# '["this" "thing"] --> ["this" "thing"]
+# '[[] []] --> [[] []]
+# '[key: value key2: value2 orphan] --> [key: value key2: value2 orphan]
+# '[8] --> [8]  --comment
+# Solved: How do we distinguish between something we're supposed to run and a plain old list? A quote? A different kind of bracket? The lack of brackets? A dialect specifier? A form, like (quote (some list)). We can abbreviate it, like '(some list).
+
+def test_int():
+    s = '8'
+    eq_(grammar['item'].parse(s), Node('item', s, 0, 1, children=[Node('number', s, 0, 1, children=[Node('int', s, 0, 1, children=[RegexNode('spaceless_int', s, 0, 1), RegexNode('_', s, 1, 1)])])]))
 
 
-def test_lexer_single_line():
-    """Make sure lexing a single line works."""
-    eq_(parse('[8]'),
-        [])
-#    eq_(parse('[funcs: [term/on-red 8 "foo"]'),
-#        [])
+def test_no_brackets_in_words():
+    """Don't let barewords suck up end brackets."""
+    assert_raises(ParseError, grammar['word'].parse, ']')
 
-# TODO:
-# * Inferred brackets around file
-# * Indentation
-# * Nested blocks
+
+def test_smoke():
+    """Smoke-test all the constructs."""
+    parse('[8]')
+    parse('[show: "hey"]')
+    parse('[frob: thing with: thong]')
+    parse('["this" "thing"]')
+    parse('[[] []]')
+    parse('[key: value key2: value2 orphan]')
+    parse('[1 + (2 + 3)]')
+    parse('[funcs: [term/on-red 8 "foo"]]')
